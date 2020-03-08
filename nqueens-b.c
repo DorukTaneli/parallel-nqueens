4ce404ef13d46e98257ee565530b5bdfb962ac0e
DorukTaneli <dtaneli16@ku.edu.tr>
2020-03-08 22:20:10 +0300

    b final commit

--- nqueens-b.c
+++ nqueens-b.c
@@ -5,7 +5,6 @@
 #include <omp.h>
 
 #define N 14
-#define MAX_LEVELS 20
 bool SOLUTION_EXISTS = false;
 
 bool can_be_placed(int board[N][N], int row, int col);
@@ -37,14 +36,14 @@ bool can_be_placed(int board[N][N], int row, int col)
 void print_solution(int board[N][N])
 {
     static int k = 1;
-    // printf("Solution #%d-\n", k++);
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //         printf(" %d ", board[i][j]);
-    //     printf("\n");
-    // }
-    // printf("\n");
+    printf("Solution #%d-\n", k++);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            printf(" %d ", board[i][j]);
+        printf("\n");
+    }
+    printf("\n");
 }
 
 bool solve_NQueens(int board[N][N], int col)
@@ -52,20 +51,19 @@ bool solve_NQueens(int board[N][N], int col)
     {
         if (col == N)
         {
-                print_solution(board);
-                SOLUTION_EXISTS = true;
-                return true;
+            //print_solution(board);
+            SOLUTION_EXISTS = true;
+            return true;
         }
         for (int i = 0; i < N; i++)
         {
             if (can_be_placed(board, i, col))
             {
-                #pragma omp task shared(SOLUTION_EXISTS)
-                {
                     board[i][col] = 1;
+                    #pragma omp task shared(SOLUTION_EXISTS) if (N < 5)
                     SOLUTION_EXISTS = solve_NQueens(board, col + 1) || SOLUTION_EXISTS;
-                }
-                board[i][col] = 0;
+                    #pragma omp taskwait
+                    board[i][col] = 0;
             }
         }
         return SOLUTION_EXISTS;
@@ -74,13 +72,16 @@ bool solve_NQueens(int board[N][N], int col)
 
 int main()
 {
-    omp_set_max_active_levels(MAX_LEVELS);
     int board[N][N];
     memset(board, 0, sizeof(board));
     double time1 = omp_get_wtime();
-
-    solve_NQueens(board, 0);
-
+#pragma omp parallel shared(SOLUTION_EXISTS, board)
+    {
+#pragma omp single
+        {
+            solve_NQueens(board, 0);
+        }
+    }
     if (SOLUTION_EXISTS == false)
     {
         printf("No Solution Exits! \n");
