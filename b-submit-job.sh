3b29a1c2acfea3a8044a8f31053b549e8ec36db3
DorukTaneli <dtaneli16@ku.edu.tr>
2020-03-08 22:18:09 +0300

    add b-submit-job.sh

---
+++ b-submit-job.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# You should only work under the /scratch/users/<username> directory.
+#
+# Example job submission script
+#
+# -= Resources =-
+#
+#SBATCH --job-name=nqueens-b
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=short
+#SBATCH --time=00:30:00
+#SBATCH --output=b-nqueens.out
+
+################################################################################
+##################### !!! DO NOT EDIT ABOVE THIS LINE !!! ######################
+################################################################################
+# Set stack size to unlimited
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+
+echo "Running Job...!"
+echo "==============================================================================="
+echo "Running compiled binary..."
+
+#serial version
+echo "Serial version..."
+./s
+
+#parallel version
+echo "B - 1 thread"
+export OMP_NUM_THREADS=1
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
+
+echo "B - 2 threads"
+export OMP_NUM_THREADS=2
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
+
+echo "B - 4 threads"
+export OMP_NUM_THREADS=4
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
+
+echo "B - 8 threads"
+export OMP_NUM_THREADS=8
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
+
+echo "B - 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
+
+echo "B - 32 threads"
+export OMP_NUM_THREADS=32
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./b
