93b7a27ab109baf0342ae028f1706e64269f169c
DorukTaneli <dtaneli16@ku.edu.tr>
2020-03-08 12:49:16 +0300

    add barlas submit job

---
+++ barlas-submit-job.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# You should only work under the /scratch/users/<username> directory.
+#
+# Example job submission script
+#
+# -= Resources =-
+#
+#SBATCH --job-name=nqueens-job
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=short
+#SBATCH --time=00:30:00
+#SBATCH --output=nqueens.out
+
+################################################################################
+##################### !!! DO NOT EDIT ABOVE THIS LINE !!! ######################
+################################################################################
+# Set stack size to unlimited
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+
+echo "Running Job...!"
+echo "==============================================================================="
+echo "Running compiled binary..."
+
+#serial version
+echo "Serial version..."
+./s
+
+#parallel version
+echo "A - 1 thread"
+export OMP_NUM_THREADS=1
+./a
+
+echo "A - 2 threads"
+export OMP_NUM_THREADS=2
+./a
+
+echo "A - 4 threads"
+export OMP_NUM_THREADS=4
+./a
+
+echo "A - 8 threads"
+export OMP_NUM_THREADS=8
+./a
+
+echo "A - 16 threads"
+export OMP_NUM_THREADS=16
+./a
+
+echo "A - 32 threads"
+export OMP_NUM_THREADS=32
+./a
